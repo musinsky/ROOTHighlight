a67c24b3c988acf1e8d9d93665f3afbcf21ae060
Jan Musinsky <musinsky@gmail.com>
2015-10-27 16:46:40 +0100

    highlight for ROOT6

---
+++ root_master/copyfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ROOTDIR=/cern/root_master
+WHERE=.
+
+# sort as git
+cp -pv $ROOTDIR/graf2d/gpad/inc/TCanvas.h $WHERE
+cp -pv $ROOTDIR/graf2d/gpad/src/TCanvas.cxx $WHERE
+cp -pv $ROOTDIR/graf3d/gl/inc/TGLHistPainter.h $WHERE
+cp -pv $ROOTDIR/graf3d/gl/src/TGLHistPainter.cxx $WHERE
+cp -pv $ROOTDIR/hist/hist/inc/TGraph.h $WHERE
+cp -pv $ROOTDIR/hist/hist/inc/TH1.h $WHERE
+cp -pv $ROOTDIR/hist/hist/inc/TVirtualGraphPainter.h $WHERE
+cp -pv $ROOTDIR/hist/hist/inc/TVirtualHistPainter.h $WHERE
+cp -pv $ROOTDIR/hist/hist/src/TGraph.cxx $WHERE
+cp -pv $ROOTDIR/hist/hist/src/TH1.cxx $WHERE
+cp -pv $ROOTDIR/hist/histpainter/inc/TGraphPainter.h $WHERE
+cp -pv $ROOTDIR/hist/histpainter/inc/THistPainter.h $WHERE
+cp -pv $ROOTDIR/hist/histpainter/src/TGraphPainter.cxx $WHERE
+cp -pv $ROOTDIR/hist/histpainter/src/THistPainter.cxx $WHERE
