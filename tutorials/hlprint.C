ac4cd35081ba04a2546a6752bb7ebefc628f4656
Jan Musinsky <musinsky@gmail.com>
2018-04-04 21:57:37 +0300

    add hlprint.C

---
+++ tutorials/hlprint.C
@@ -0,0 +1,23 @@
+#include <TCanvas.h>
+#include <TH1.h>
+
+void PrintInfo(TVirtualPad *pad, TObject *obj, Int_t x, Int_t y)
+{
+   TH1F *h = (TH1F *)obj;
+
+   if (!h->IsHighlight()) // after highlight disabled
+      h->SetTitle("highlight disable");
+   else
+      h->SetTitle(TString::Format("bin[%03d] (%5.2f) content %g", x,
+                                  h->GetBinCenter(x), h->GetBinContent(x)));
+   pad->Update();
+}
+
+void hlprint()
+{
+   // TQObject::Connect("TCanvas", "Highlighted(TVirtualPad*,TObject*,Int_t,Int_t)",
+   //                   0, 0, "PrintInfo(TVirtualPad*,TObject*,Int_t,Int_t)");
+
+   // or simplified
+   gPad->GetCanvas()->HighlightConnect("PrintInfo(TVirtualPad*,TObject*,Int_t,Int_t)");
+}
